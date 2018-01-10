6759a2ce90b462e43c514830b2bf000b0981dcba
Csilla Varga <pom.csilla@gmail.com>
2018-01-10 17:41:18 +0100

    initial

---
+++ OldAppToCore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace OldAppToCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            reversed = Reverse(reversed);
+            Console.WriteLine(reversed);
+            Console.ReadLine();
+        }
+
+        public static string Reverse(string reverseString)
+        {
+            string rightOrder = "";
+            for (int i = (reverseString.Length - 1); i >= 0; i--)
+            {
+                rightOrder += reverseString[i];
+            }
+            return rightOrder;
+        }
+    }
+}
